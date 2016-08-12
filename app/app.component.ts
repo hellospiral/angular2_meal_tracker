2b75d41fbb2dbba49498c73823fee606e37e53c7
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-12 08:35:20 -0700

    set up development environment

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: "my-app",
+  template: `
+    <div class="container">
+      <h1>Welcome to meal tracker!</h1>
+    </div>
+  `
+})
+export class AppComponent{
+
+}

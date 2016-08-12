e6814972538e2c4581a859d63e2e10e8cdcc2cea
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-12 10:53:42 -0700

    create calorie-pipe.ts; get it functioning in app

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,16 +1,23 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 import { MealComponent } from './meal.component';
 import { NewMealComponent } from './new-meal.component';
 import { EditMealComponent } from './edit-meal.component';
+import { CaloriePipe } from './calorie.pipe';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
   directives: [MealComponent, NewMealComponent, EditMealComponent],
+  pipes: [CaloriePipe],
   template: `
+    <select (change)="onChange($event.target.value)">
+      <option value = "High">High Calorie Meals</option>
+      <option value = "Low">Low Calorie Meals</option>
+      <option value = "All">All Meals</option>
+    </select>
     <meal-display
-      *ngFor = '#currentMeal of mealList'
+      *ngFor = '#currentMeal of mealList | calorie:selectedOption'
       [meal]="currentMeal"
       (click)="mealClicked(currentMeal)">
     </meal-display>
@@ -22,6 +29,7 @@ import { EditMealComponent } from './edit-meal.component';
 export class MealListComponent {
   public mealList: Meal[];
   public selectedMeal: Meal;
+  public selectedOption: string = "All";
   createNewMeal(args: string[]): void {
     this.mealList.push(
       new Meal(args[0], args[1], parseInt(args[2]), this.mealList.length)
@@ -29,5 +37,8 @@ export class MealListComponent {
   }
   mealClicked(clickedMeal: Meal) {
     this.selectedMeal = clickedMeal;
+  }
+  onChange(option: string) {
+    this.selectedOption = option;
   }
 }
